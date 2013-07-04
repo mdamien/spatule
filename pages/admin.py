0999e6b9adb0c7a741a60271f5e5be8a4e9190ba
Damien <damienlog@gmail.com>
2013-07-04 07:22:39 +0200

    added articles in pages

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from pages.models import Page,HomePage,SliderImage
+from pages.models import Page,HomePage,SliderImage,Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('page','position','title')
 
 admin.site.register(Page)
 admin.site.register(HomePage)
 admin.site.register(SliderImage)
+admin.site.register(Article,ArticleAdmin)
--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ class Page(models.Model):
 	title = models.CharField(max_length=400)
 	content = HTMLField()
 	url = models.CharField(max_length=400)
-	image = models.ImageField(upload_to="pages")
+	image = models.ImageField(upload_to="pages",blank=True)
 
 	def __unicode__(self):
 		return self.title
@@ -32,3 +32,15 @@ class SliderImage(models.Model):
 
 	def __unicode__(self):
 		return self.get_category_display() + ": " + str(self.pk)
+
+class Article(models.Model):
+	title = models.CharField(max_length=400)
+	content = HTMLField()
+	image = models.ImageField(upload_to="articles",blank=True)
+	position = models.IntegerField()
+	page = models.ForeignKey('Page')
+	class Meta:
+		ordering = ['page', 'position']
+
+	def __unicode__(self):
+		return self.page.title + ": " + self.title + " ("+str(self.position)+")"
